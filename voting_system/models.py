21773608fdb7493584270905c77400b151077122
ioana <ioana.sbob@gmail.com>
2016-12-22 09:26:12 +0200

    model update

--- voting_system/models.py
+++ voting_system/models.py
@@ -17,13 +17,13 @@ class Test(models.Model):
 
 class Admin(models.Model):
     id = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=45)
-    lastName = models.CharField(max_length=45)
-    userName = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    user_name = models.CharField(max_length=30)
     password_hash = models.CharField(max_length=300)
     email = models.CharField(max_length=60)
     # foreign key
-    roles = models.ManyToManyField(Role)
+    roles = models.ManyToManyField(Role, through='AdminRole')
     class Meta:
         db_table = 'admins'
         app_label = 'admin'
@@ -35,6 +35,15 @@ class Role(models.Model):
         db_table = 'roles'
         app_label = 'admin'
 
+class AdminRole(models.Model):
+    id = models.IntegerField(primary_key=True)
+    admin = models.ForeignKey(Admin, db_column='admin_id')
+    role = models.ForeignKey(Role, db_column='role_id')
+
+    class Meta:
+        db_table = 'admin_roles'
+        app_label = 'admin'
+
 class Party(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
@@ -44,38 +53,56 @@ class Party(models.Model):
 
 class Candidate(models.Model):
     id = models.IntegerField(primary_key=True)
-    firstName = models.CharField(max_length=45)
-    lastName = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
     email = models.CharField(max_length=60)
     # foreign key
-    candidate = models.ForeignKey(Party, on_delete=models.CASCADE, db_column='candidate_id')
+    party = models.ForeignKey(Party, on_delete=models.CASCADE, db_column='party_id')
     class Meta:
         db_table = 'candidates'
         app_label = 'admin'
 
 class Election(models.Model):
     id = models.IntegerField(primary_key=True)
-    startDate = models.DateField(default=timezone.now)
-    endDate = models.DateField()
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField()
     #uninominal or secret voting =>
-    # if uninominal true, if secret false
-    uninominalVoting = models.BooleanField()
+    # if uninominal voting = true, if party list = false
+    uninominal_voting = models.BooleanField()
+    # if secret voting, is true
+    secret_voting = models.BooleanField()
     #foreign keys
-    parties = models.ManyToManyField(Party)
-    candidates = models.ManyToManyField(Candidate)
+    parties = models.ManyToManyField(Party, through='ElectionParty')
+    candidates = models.ManyToManyField(Candidate, through='ElectionCandidate')
     class Meta:
         db_table = 'elections'
         app_label = 'admin'
 
+class ElectionParty(models.Model):
+    id = models.IntegerField(primary_key=True)
+    election = models.ForeignKey(Election, db_column='election_id')
+    party = models.ForeignKey(Party, db_column='party_id')
+    class Meta:
+        db_table = 'election_parties'
+        app_label = 'admin'
+
+class ElectionCandidate(models.Model):
+    id = models.IntegerField(primary_key=True)
+    election = models.ForeignKey(Election, db_column='election_id')
+    candidate = models.ForeignKey(Party, db_column='candidate_id')
+    class Meta:
+        db_table = 'election_candidates'
+        app_label = 'admin'
+
 class VoterCode(models.Model):
     id = models.IntegerField(primary_key=True)
-    verifiedDate = models.DateTimeField()
-    invalidatedDate = models.DateTimeField()
-    registeredNumber = models.IntegerField()
+    verified_date = models.DateTimeField()
+    invalidated_date = models.DateTimeField()
+    registered_number = models.CharField()
     # should set up minumum = maximum length here 
     code = models.CharField(max_length=15)
     # foreign keys
-    election = models.ForeignKey(Election, on_delete=models.CASCADE, db_column='elecion_id')
+    election = models.ForeignKey(Election, on_delete=models.CASCADE, db_column='election_id')
     region = models.ForeignKey(Region, on_delete=models.CASCADE, db_column='region_id')
     class Meta:
         db_table = 'voter_codes'
@@ -95,7 +122,7 @@ class Region(models.Model):
 
 class VoterAuth(models.Model):
     id = models.IntegerField(primary_key=True)
-    regionNumber = models.IntegerField()
+    registration_number = models.IntegerField()
     password_hash = models.CharField(max_length=300)
     class Meta:
         db_table = 'voter_auth'
@@ -108,27 +135,27 @@ class VoterAuth(models.Model):
 
 ### In reality, 660
 
-# Region id names of the table MUST correspond with the info inside the
-# region table in the main database.
+# Class member names MUST correspond with the info inside the
+# region votes table in the main database.
 # Otherwise, they can't be Queried!!!
-class Region1Name(models.Model):
+class Region1Votes(models.Model):
     id = models.IntegerField(primary_key=True)
-    electionId = models.IntegerField()
-    candidateId = models.IntegerField()
-    candidateCount = models.IntegerField()
+    election_id = models.IntegerField()
+    candidate_id = models.IntegerField()
+    candidate_count = models.IntegerField()
 
     class Meta:
-        db_table = 'region1_name'
+        db_table = 'region1_votes'
         app_label = 'reg1'
 
-class Region2Name(models.Model):
+class Region2Votes(models.Model):
     id = models.IntegerField(primary_key=True)
-    electionId = models.IntegerField()
-    candidateId = models.IntegerField()
-    candidateCount = models.IntegerField()
+    election_id = models.IntegerField()
+    candidate_id = models.IntegerField()
+    candidate_count = models.IntegerField()
 
     class Meta:
-        db_table = 'region2_name'
+        db_table = 'region2_votes'
         app_label = 'reg2'
 
 
