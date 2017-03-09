cc5a30857a5fa8a091e6f906cb818c5a6726c1ec
ioana <ioana.sbob@gmail.com>
2017-03-09 14:47:52 +0000

    static page - homepage

--- voting_system/urls.py
+++ voting_system/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     #admin interface
     url(r'^regions/$', views.regions, name='regions'),
     url(r'^regions/populate$', views.populate_regions, name='populate_regions'),
+
+    #voter interface
+    url(r'^$', views.homepage, name='homepage'),
 ]
