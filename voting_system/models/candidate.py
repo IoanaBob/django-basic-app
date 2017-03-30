82a6df8540b8e8477556942396ff31f9c3f2eab1
sammantle14 <sam_mantle14@yahoo.co.uk>
2017-03-30 13:20:10 +0100

    validation for models

--- voting_system/models/candidate.py
+++ voting_system/models/candidate.py
@@ -1,11 +1,13 @@
 from django.db import models
 from .party import Party
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 class Candidate(models.Model):
     id = models.IntegerField(primary_key=True)
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-    email = models.CharField(max_length=60)
+    first_name = models.CharField(max_length=45, validators=[RegexValidator(regex="[a-zA-Z]+", message="Candidate name must only contain the following characters: a-z	A-Z	àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð, . ‘ -", code="invalid name")])
+    last_name = models.CharField(max_length=45, validators=[RegexValidator(regex="[a-zA-Z]+", message="Candidate name must only contain the following characters: a-z	A-Z	àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð, . ‘ -", code="invalid name")])
+    email = models.EmailField(max_length=60)
     # foreign key
     party = models.ForeignKey(Party, on_delete=models.CASCADE, db_column='party_id')
 
