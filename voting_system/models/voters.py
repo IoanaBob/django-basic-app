b34cc730edd27237701a70b378895bea78b0ff3f
ioana <ioana.sbob@gmail.com>
2017-04-15 13:53:40 +0300

    copy api data into voters function

--- voting_system/models/voters.py
+++ voting_system/models/voters.py
@@ -20,3 +20,20 @@ class Voter(models.Model):
     class Meta:
         db_table = 'voters'
         app_label = 'people'
+
+    def geneate_voter_id():
+    	generated_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(15))
+        if not Voter.objects.filter(code = generated_id).exists():
+            return generated_id
+        else:
+            generate_voter_id()
+
+    def populate_voters():
+        data = requests.get(url = "http://t2a.co/rest/?output=json&method=person_search&api_key=test").json()
+        data = data["person_list"]
+        i = 1
+        for person in data:
+            voterid = VoterCode.generate_voter_id()
+            entry = Voter(id=i, voter_id=voterid, title = person["title"], forname = person["forname"], surname = person["surname"], name_single_line = person["name_single_line"], telephone_number = person["telephone_number"], line_1 = person["line_1"], line_2 = person["line_2"], line_3 = person["line_3"], place = person["place"], town = person["town"], postcode = person["postcode"], addr_single_line = person["addr_single_line"])
+            entry.save()
+            i += 1
