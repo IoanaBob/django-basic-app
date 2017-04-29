272b5d488dd65c464d70e7c78d57e31a49519a3d
sammantle14 <sam_mantle14@yahoo.co.uk>
2017-04-30 00:40:31 +0100

    update party name length

--- voting_system/models/party.py
+++ voting_system/models/party.py
@@ -4,7 +4,7 @@ from django.core.validators import RegexValidator
 
 class Party(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30, validators=[RegexValidator(regex="[a-zA-Z]+", message="Party name must only contain the following characters: a-z	A-Z	àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð, . ‘ -", code="invalid name")])
+    name = models.CharField(max_length=40, validators=[RegexValidator(regex="[a-zA-Z]+", message="Party name must only contain the following characters: a-z	A-Z	àáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð, . ‘ -", code="invalid name")])
     class Meta:
         db_table = 'parties'
         app_label = 'admin'
